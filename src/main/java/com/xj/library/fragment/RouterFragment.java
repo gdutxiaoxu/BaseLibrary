c746948b7832931ac20da4af91fe2d937edc6df3
xujun <xujun2@bigo.sg>
2019-04-26 12:08:35 +0800

    封装处理 onActivityResult 的基类

---
+++ src/main/java/com/xj/library/fragment/RouterFragment.java
@@ -0,0 +1,57 @@
+package com.xj.library.fragment;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.SparseArray;
+
+import java.util.Random;
+
+/**
+ * Created by jun xu on 19-3-18.
+ */
+public class RouterFragment extends Fragment {
+
+    private SparseArray<ActivityResultHelper.Callback> mCallbacks = new SparseArray<>();
+    private Random mCodeGenerator = new Random();
+
+    public RouterFragment() {
+        // Required empty public constructor
+    }
+
+    public static RouterFragment newInstance() {
+        return new RouterFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+
+    public void startActivityForResult(Intent intent, ActivityResultHelper.Callback callback) {
+        int requestCode = makeRequestCode();
+        mCallbacks.put(requestCode, callback);
+        startActivityForResult(intent, requestCode);
+    }
+
+    private int makeRequestCode() {
+        int requestCode;
+        int tryCount = 0;
+        do {
+            requestCode = mCodeGenerator.nextInt(0x0000FFFF);
+            tryCount++;
+        } while (mCallbacks.indexOfKey(requestCode) >= 0 && tryCount < 10);
+        return requestCode;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        ActivityResultHelper.Callback callback = mCallbacks.get(requestCode);
+        mCallbacks.remove(requestCode);
+        if (callback != null) {
+            callback.onActivityResult(resultCode, data);
+        }
+    }
+}
